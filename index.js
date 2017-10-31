b52f446fdb86aa7d4367c6c19dcc577798481a91
Christian Wang <christian.gnaw@gmail.com>
2017-10-31 12:10:22 -0500

    Added working api and basic content update grab

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
-//This is our api key for Dark Sky
-let key = "b1183e501b548d0852a69352f127c8d6";
+//This is our api key for OpenWeatherMap
+let key = "b370a49d76395c80b0b3652da6a0696f";
 
 /**
- * Returns the parsed request to Dark Sky's API
+ * Returns the parsed request to OpenWeatherMap's API
  * @param string apiKey
  * @param object {latitude, longitude}
  **/
-async function getForecast(apiKey, location) {
-  let response = await fetch("https://api.darksky.net/forecast/" + apiKey + "/" + location.latitude + "," + location.longitude);
+async function getForecast(key, cityName) {
+  let response = await fetch("http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&APPID=" + key + "&units=metric");
   // only proceed once promise is resolved
   let data = await response.json();
   // only proceed once second promise is resolved
   return data;
 }
 
-getForecast(key, {latitude: 42, longitude: 42}).then(function(data) {
-  console.log(data);
+let c = document.getElementById("content");
+
+getForecast(key, "Toronto, ca").then(function(data) {
+  c.innerHTML += '<h2>'+data.weather[0].description+'</h2>';
+  c.innerHTML += '<h3>Temperature: ' + data.main.temp+' °C</h3>';
+  c.innerHTML += '<h3>Pressure: ' + data.main.pressure+' kPa</h3>';
 });
